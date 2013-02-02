786d9be103208f5a973ac499af858cc9cb45d0fe
Brent's Retina Mac <bweber@redsparklabs.com>
2013-02-01 19:17:36 -0600

    New Site Stuff
    Signed-off-by: Brent's Retina Mac <bweber@redsparklabs.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpinupBlog::Application.config.secret_token = 'c837f5c04e55fef55753ad55cc4322e51d2cbf1f0da927ce8d396df993d1c95d647b540ce9bbbc5ff4db6e5d2a406f6b4a161bd068417d721d3ffa992f3f5238'
