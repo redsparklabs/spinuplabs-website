de2400c357e4b471ad32b0d0ce38e60e200aae71
Brent's Retina Mac <bweber@redsparklabs.com>
2013-07-01 11:25:04 -0600

    Adding Page Title to head
    Signed-off-by: Brent's Retina Mac <bweber@redsparklabs.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :recent_posts
+  helper_method :set_metadata
 
   def recent_posts
     @recent_posts = Post.order('post_date DESC').limit(10)
     @budgets = ['$10k-25k','$25k-50k','$50k-100k','$100k-250k','$250k+']
   end
+
+  def set_metadata
+    if @post.meta_title?
+      @page_title = @post.meta_title
+    elsif !@page_title
+      @page_title = "Full Service Web Development"
+    end
+  end
+      
 
   protected
     def authenticate
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,25 @@
 class PagesController < ApplicationController
   require 'kramdown'
   def index
+    @page_title = "Home -> Full Service Web Consulting Company"
   end
 
   def about
+    @page_title = "About Us -> Who we are and why we do what we do"
   end
 
   def contact
+    @page_title = "Contact Us -> How can we help you?"
     @contact = Contact.new
   end
 
   def our_process
+    @page_title = "Process -> Building blocks for a successful startup!"
   end
 
   def blog
     @posts = Post.order('post_date DESC').page params[:page]
+    @page_title = "Blog -> Our opinions about hacking and hustling for Startups!"
     #@post_pages, @posts = paginate(:posts, :order => 'post_date DESC', :per_page => 2)
   end
 end
